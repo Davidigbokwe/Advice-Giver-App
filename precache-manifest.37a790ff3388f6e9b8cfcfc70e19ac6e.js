45cb2ebbf5a7e0522525d74400e00b6e6e7d94f3
Fullstack Developer <delosmusicc@gmail.com>
2020-09-08 09:52:58 -0700

    Updates

---
+++ precache-manifest.37a790ff3388f6e9b8cfcfc70e19ac6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5836a671c6bc2f4502fbd1dd0edb466",
+    "url": "/Advice-Giver-App/static/media/city1.a5836a67.jpg"
+  },
+  {
+    "revision": "49fc3719b615767f4920",
+    "url": "/Advice-Giver-App/static/js/runtime~main.db7f8bf2.js"
+  },
+  {
+    "revision": "f7450c5e4807d208069e",
+    "url": "/Advice-Giver-App/static/js/main.ff1b4b87.chunk.js"
+  },
+  {
+    "revision": "70d6428f1754b3a7520b",
+    "url": "/Advice-Giver-App/static/js/2.cdf93283.chunk.js"
+  },
+  {
+    "revision": "f7450c5e4807d208069e",
+    "url": "/Advice-Giver-App/static/css/main.9934a973.chunk.css"
+  },
+  {
+    "revision": "691ca9c70b9d564c4b08f53ccef5a8e7",
+    "url": "/Advice-Giver-App/index.html"
+  }
+];
